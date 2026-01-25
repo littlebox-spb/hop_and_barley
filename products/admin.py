6fe4b9c1ea91443d31b1371a1532f79c2a2f9648
Alexandr Altukhov <sam@littlebox.spb.ru>
2026-01-26 02:03:31 +0300

    Add models. Create docker containers.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from products.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "price", "stock", "is_active"]
+    list_filter = ["category", "is_active"]
+    prepopulated_fields = {"slug": ("name",)}
--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    parent = models.ForeignKey(
+        "self", blank=True, null=True, related_name="children", on_delete=models.CASCADE
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify.name
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify.name
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
