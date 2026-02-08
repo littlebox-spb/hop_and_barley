58dac9670e852677b7b64a257e1fc9d783e8902e
Alexandr Altukhov <sam@littlebox.spb.ru>
2026-02-08 14:13:55 +0300

    Product and Orders api

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = (os.getenv("ALLOWED_HOSTS"),)
+# ALLOWED_HOSTS = (os.getenv("ALLOWED_HOSTS"), )
+ALLOWED_HOSTS = ("127.0.0.1", "localhost")
 
 
 # Application definition
